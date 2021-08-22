e6da75fdf19b2e9a9a1e46f9100c182d2865b0e7
bankole <bankoleibrahim96@gmail.com>
2021-08-22 02:26:04 +0100

    changes

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggle = document.querySelector(".menu");
 const navigation = document.querySelector(".nav");
 const dropDown = document.querySelector(".drop");
+const buy = document.querySelector(".buy");
+const drop = document.querySelector(".drop-down");
 
 toggle.addEventListener("click", () => {
   toggle.classList.toggle("active");
@@ -13,3 +15,20 @@ window.addEventListener("scroll", function () {
 });
 
 dropDown.classList.add("hidden");
+let show = false;
+buy.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!show) {
+    dropDown.classList.remove("hidden");
+    buy.classList.toggle(drop.after(dropDown));
+    buy.style.backgroundColor = "blue";
+    buy.style.color = "white";
+
+    show = true;
+  } else {
+    dropDown.classList.add("hidden");
+    buy.style.backgroundColor = "var(--color-tertiary)";
+    buy.style.color = "black";
+    show = false;
+  }
+});
